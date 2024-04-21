22caa944609b3e50d1f966804aa0879fa3428087
Caleb <support@tabslite.com>
2024-04-21 18:52:28 -0500

    Add strings for search bar

--- app/src/main/java/com/gbros/tabslite/compose/tabsearchbar/TabsSearchBar.kt
+++ app/src/main/java/com/gbros/tabslite/compose/tabsearchbar/TabsSearchBar.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,11 +63,11 @@ fun TabsSearchBar(
         trailingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
-                contentDescription = "Search"
+                contentDescription = stringResource(id = R.string.app_action_description_search)
             )
         },
         placeholder = {
-            Text(text = "Search TabsLite")
+            Text(text = stringResource(id = R.string.app_action_search))
         },
         modifier = modifier
     ) {
