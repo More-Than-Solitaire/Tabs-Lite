00897a7d49b9e924f59e12d92450c187ac4afffb
Caleb Gregory <caleb.gregory@biotronik.com>
2025-04-01 14:45:55 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,12 +45,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_23
-        targetCompatibility = JavaVersion.VERSION_23
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "23"
+        jvmTarget = "21"
     }
 
     dependenciesInfo {
@@ -68,14 +68,14 @@ repositories {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.10.0")  // Compose Integration with activities
+    implementation("androidx.activity:activity-compose:1.10.1")  // Compose Integration with activities
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.compose.material3:material3:1.3.1")  // Material Design 3
     implementation("androidx.compose.material:material-icons-core:1.7.8")
     implementation("androidx.compose.runtime:runtime-livedata:1.7.8")
     // Compose Integration with LiveData
     implementation("androidx.compose.ui:ui-tooling-preview")  // Android Studio Preview Support
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
@@ -84,9 +84,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")  // Compose Integration with ViewModels
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.navigation:navigation-compose:2.8.7")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.8.1")
+        classpath("com.android.tools.build:gradle:8.8.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.7")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.9")
     }
 }
 
