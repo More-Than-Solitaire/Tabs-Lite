be490947b4694ce689a981475d68d805502cd54e
Caleb Gregory <caleb.gregory@biotronik.com>
2025-04-22 13:59:42 -0400

    Update gradle to 8.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.1")
+        classpath("com.android.tools.build:gradle:8.9.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.9")
     }
