f5f9efc6bb91045f253c8c9ece511dd5afa2172f
Caleb Gregory <caleb.gregory@biotronik.com>
2025-10-10 18:26:59 -0400

    Bump version to 3.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         minSdk = 24
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 3700
-        versionName = "3.7.0"
+        versionCode = 3800
+        versionName = "3.8.0"
     }
     buildTypes {
         release {
