6950f83fe8e35789f070aab8d1c96fb72d921c9e
Caleb Gregory <caleb.gregory@biotronik.com>
2026-01-25 17:11:20 -0500

    Use textbutton style chord button

--- app/src/main/java/com/gbros/tabslite/view/tabview/ChordButton.kt
+++ app/src/main/java/com/gbros/tabslite/view/tabview/ChordButton.kt
@@ -1,14 +1,13 @@
 package com.gbros.tabslite.view.tabview
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
@@ -25,15 +24,12 @@ fun ChordButton(
     BasicText(
         text = text,
         modifier = modifier
-            .background(
-                color = MaterialTheme.colorScheme.primary,
-                shape = RoundedCornerShape(4.dp)
-            )
             .clickable(onClick = onClick)
             .padding(horizontal = 3.dp, vertical = 0.dp),
         style = TextStyle(
-            color = MaterialTheme.colorScheme.onPrimary,
-            fontSize = TextUnit(fontSizeSp, TextUnitType.Sp)
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = TextUnit(fontSizeSp, TextUnitType.Sp),
+            fontWeight = FontWeight.Bold
         )
     )
 }
